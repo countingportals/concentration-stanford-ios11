21b94f9d9c3854fa6e83f5c844bf3ef78b0355d4
Ashley Raines <ashley.a.raines@gmail.com>
2018-09-21 21:02:51 -0500

    Creating Xcode project

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ashley Andersen on 9/21/18.
+//  Copyright © 2018 Ashley Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
